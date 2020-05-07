76120bb765bbf0e7cf3033e1528bae32a9195dd1
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2020-05-07 22:16:40 +0530

    tool: add clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require( 'path' );
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const OptimizeCssAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
+const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
 
 module.exports = {
 	entry: {
@@ -36,6 +37,7 @@ module.exports = {
 		],
 	},
 	plugins: [
+		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin( {
 			template: 'index.html',
 			inject: 'body',
